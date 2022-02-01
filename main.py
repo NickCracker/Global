aebf11690beecf7de2301b00866bb92b4e0a658d
cracker <nickocruz4513@gmail.com>
2022-02-01 13:38:34 -0300

    cambio de paleta

--- main.py
+++ main.py
@@ -1,80 +1,56 @@
 from flask import Flask, redirect, render_template, request, url_for
-from flask_mysqldb import MySQL
 import pyodbc
 
-"""
-    En la tabla INBODEGA estan los codigos de bodega mediante CODIGO_BOD
-    
-"""
-
+#Instanciacion del modulo Flask
 app = Flask(__name__)
 
-#app.config['MYSQL_HOST']='localhost'
-#app.config['MYSQL_USER']='root'
-#app.config['MYSQL_PASSWORD']=''
-#app.config['MYSQL_DB']='prueba'
-#mysql = MySQL(app)
+#Datos para la conexion con SQL Server
+servidor="192.168.0.97"
+base="Global"
+usuario="globalsql"
+contraseña="010101zxAS"
 
-s="192.168.0.97"
-d="Global"
-u="globalsql"
-p="010101zxAS"
-
+#Esta funcion permite conectar a la base de datos SQL Server y retorna un curso
 def conectar_base():
-    conexion = pyodbc.connect('DRIVER=ODBC Driver 17 for SQL server;SERVER={0};DATABASE={1};UID={2};PWD={3}'.format(s,d,u,p))
+    conexion = pyodbc.connect('DRIVER=ODBC Driver 17 for SQL server;SERVER={0};DATABASE={1};UID={2};PWD={3}'.format(servidor,base,usuario,contraseña))
     cursor = conexion.cursor()
     return cursor
 
+#Redireccion al login 
 @app.route('/')
 def Login():
     return render_template('login.html')
 
+#Redireccion a la pagina de busqueda y consulta de datos
 @app.route('/view')
 def Buscador():
-    
-    #MYSQL cur= mysql.connection.cursor()
-    #MYSQL cur.execute('SELECT * FROM vista')
-
     cur = conectar_base()
     cur.execute('SELECT * FROM inve_web')
-    
     data = cur.fetchall()
-    #MYSQL mysql.connection.commit()
-    
     for dato in data:
         dato[1]=int(dato[1])
         dato[2]=int(dato[2])
-        
     return render_template('buscador.html', valores = data)
 
+#Logica del login, redirecciona al login si hay error y da acceso al buscador si se registra bien
 @app.route('/Permitir_acceso', methods=['POST'])
 def Permitir_acceso():
     if request.method == 'POST':
         usuario = request.form['usuario']
         contraseña = request.form['contraseña']
-        
         cur = conectar_base()
-        
-        #MYSQL cur= mysql.connection.cursor()
-        #cur.execute('SELECT * FROM usuarios WHERE USUARIO=\''+usuario+'\' AND CONTRASEÑA=\''+contraseña+'\';')
         cur.execute('SELECT * FROM web_user_inventory WHERE USUARIO=\''+usuario+'\' AND CONTRASEÑA=\''+contraseña+'\';')
-        
         data = cur.fetchall()
-        #COMPROBAR EN BASE DE DATOS DE USUARIOS
         if data is None :
             return redirect(url_for('Login'))
         else:
             return redirect(url_for('Buscador'))
 
+#Redirecciona a la pagina de detalles 
 @app.route('/detalle/<string:id>')
 def Mostrar_detalle(id):
-    
     cur = conectar_base()
     cur.execute('SELECT * FROM inve_web where CODIGO = \''+id+'\';')
-    
-    #cur= mysql.connection.cursor()
-    #cur.execute('SELECT * FROM vista where CODIGO = \''+id+'\';')
-    
     data = cur.fetchall()
     return render_template('detalle.html',detalles=data[0])
 
@@ -88,7 +64,6 @@ def buscar():
         for dato in data:
             dato[1]=int(dato[1])
             dato[2]=int(dato[2])
-        
         return render_template('buscador.html', valores = data)
         
 
